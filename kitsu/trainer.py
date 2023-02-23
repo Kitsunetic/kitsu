1780c25d782b024cdca915449403ba6e90bec6d7
kitsunetic <jh.shim.gg@gmail.com>
2023-02-23 23:19:26 +0900

    update StepTrainerEMA - evaluation for EMA

--- kitsu/trainer.py
+++ kitsu/trainer.py
@@ -630,6 +630,7 @@ class StepTrainerEMA(StepTrainer):
         self.ema_decay = ema_decay
 
         self._ema_state = False
+        self.best_ema_epoch = -1
 
     @contextmanager
     def ema_state(self, activate=True):
@@ -657,18 +658,74 @@ class StepTrainerEMA(StepTrainer):
     def save(self, out_path):
         data = {
             "optim": self.optim.state_dict(),
-            "model": self.model_src.state_dict(),
-            "model_ema": self.model_ema.state_dict(),
+            "model": self.model.state_dict(),
             "epoch": self.epoch,
         }
         torch.save(data, str(out_path))
 
     @torch.no_grad()
+    def evaluation_ema(self, *o_lst):
+        # self.step_sched(o_lst[0][self.monitor], is_on_epoch=True)
+
+        improved = False
+        if self.rankzero:  # scores are not calculated in other nodes
+            flag = ""
+            if (
+                (self.small_is_better and o_lst[0][self.monitor] < self.best_ema)
+                or (not self.small_is_better and o_lst[0][self.monitor] > self.best_ema)
+                or (
+                    self.sample_at_least_per_epochs is not None
+                    and (self.epoch - self.best_ema_epoch) >= self.sample_at_least_per_epochs
+                )
+            ):
+                if self.small_is_better:
+                    if self.best_ema > o_lst[0][self.monitor]:
+                        self.best_ema = o_lst[0][self.monitor]
+                        improved = True
+                else:
+                    if self.best_ema < o_lst[0][self.monitor]:
+                        self.best_ema = max(self.best_ema, o_lst[0][self.monitor])
+                        improved = True
+
+                self.best_ema_epoch = self.epoch
+                self.save(self.args.exp_path / "best_ema_ep{:06d}.pth".format(self.epoch))
+                saved_files = sorted(list(self.args.exp_path.glob("best_ema_ep*.pth")))
+                if len(saved_files) > self.num_saves:
+                    to_deletes = saved_files[: len(saved_files) - self.num_saves]
+                    for to_delete in to_deletes:
+                        utils.io.try_remove_file(str(to_delete))
+
+                flag = "*"
+                improved = self.epoch > self.epochs_to_save or self.args.debug or not self.save_only_improved
+
+            msg = f"Step-EMA[%06d/%06d]" % (self.epoch, self.args.epochs)
+            msg += f" {self.monitor}[" + ";".join([o._get(self.monitor) for o in o_lst]) + "]"
+            msg += " (best:%.4f%s)" % (self.best_ema, flag)
+
+            keys = reduce(lambda x, o: x | set(o.data.keys()), o_lst, set())
+            keys = sorted(list(filter(lambda x: x != self.monitor, keys)))
+
+            for k in keys:
+                msg += f" {k}[" + ";".join([o._get(k) for o in o_lst]) + "]"
+
+            self.log.info(msg)
+            self.log.flush()
+
+        # share improved condition with other nodes
+        if self.ddp:
+            improved = torch.tensor([improved], device="cuda")
+            dist.broadcast(improved, 0)
+
+        return improved
+
+    @torch.no_grad()
     def stage_eval(self, o_train):
         o_valid = self.valid_epoch(self.dl_valid)
+        improved = self.evaluation(o_valid, o_train)
+
         with self.ema_state():
             o_valid_ema = self.valid_epoch(self.dl_valid)
-        improved = self.evaluation(o_valid, o_valid_ema, o_train)
+            improved = self.evaluation(o_valid_ema, o_train)
 
         if improved:
             self.sample()
