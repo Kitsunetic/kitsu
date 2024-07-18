8e0f47aa292329f85fbc3fa92d6fe69d27cf0da6
Kitsunetic <jh.shim.gg@gmail.com>
2024-07-18 17:50:14 +0900

    specify output dtype for dist functions

--- kitsu/utils/dist.py
+++ kitsu/utils/dist.py
@@ -2,20 +2,20 @@ import torch
 import torch.distributed as dist
 
 
-def safe_all_reduce(x, reduce_op=dist.ReduceOp.SUM):
+def safe_all_reduce(x, reduce_op=dist.ReduceOp.SUM) -> torch.Tensor:
     if dist.is_initialized():
         dist.all_reduce(x, reduce_op)
     return x
 
 
-def safe_all_mean(x):
+def safe_all_mean(x) -> torch.Tensor:
     x = safe_all_reduce(x)
     if dist.is_initialized():
         x /= dist.get_world_size()
     return x
 
 
-def safe_all_gather(x, dim=0):
+def safe_all_gather(x, dim=0) -> torch.Tensor:
     if dist.is_initialized():
         xs = [torch.empty_like(x) for _ in range(dist.get_world_size())]
         dist.all_gather(xs, x)
@@ -28,6 +28,7 @@ def safe_barrier():
         dist.barrier()
 
 
-def safe_broadcast(x, src):
+def safe_broadcast(x, src) -> torch.Tensor:
     if dist.is_initialized():
         dist.broadcast(x, src)
+    return x
