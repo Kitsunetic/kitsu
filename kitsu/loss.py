2c0361630333f7b98e34bb1bdd46e903c2638e7b
Kitsunetic <jh.shim.gg@gmail.com>
2024-09-14 00:46:34 +0900

    add label smoothing for focal loss

--- kitsu/loss.py
+++ kitsu/loss.py
@@ -1,11 +1,27 @@
 import torch as th
 import torch.nn.functional as F
+from torch import Tensor
 
 
-def focal_loss(inputs, targets, alpha=1, gamma=2, reduction="mean"):
-    ce_loss = F.cross_entropy(inputs, targets, reduction="none")
-    p_t = ce_loss.neg().exp()
-    focal_loss = alpha * (1 - p_t) ** gamma * ce_loss
+def focal_loss(inputs: Tensor, targets: Tensor, alpha=1, gamma=2, reduction="mean", label_smoothing=0.0):
+    """
+    Args:
+        inputs (Tensor): b c
+        targets (Tensor): b, int64
+    """
+    if label_smoothing <= 0.0:
+        ce_loss = F.cross_entropy(inputs, targets, reduction="none")
+        p_t = ce_loss.neg().exp()
+        focal_loss = alpha * (1 - p_t) ** gamma * ce_loss
+    else:
+        C = inputs.size(-1)
+        if label_smoothing > 0.0:
+            targets = (1 - label_smoothing) * F.one_hot(targets, C) + label_smoothing / C
+
+        log_probs = th.log_softmax(inputs, -1)
+        ce_loss = -(targets * log_probs).sum(-1)  # b
+        p_t = ce_loss.neg().exp()
+        focal_loss = alpha * (1 - p_t) ** gamma * ce_loss
 
     if reduction == "mean":
         return focal_loss.mean()
