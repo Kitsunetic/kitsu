be7b7b4cbe15d86d715f0997c1fb8c5e9d2cf19e
Kitsunetic <jh.shim.gg@gmail.com>
2024-09-25 19:25:44 +0900

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="kitsu",
-    version="0.1.2.6",
+    version="0.1.2.7",
     description="",
     author="Kitsunetic",
     author_email="jh.shim.gg@gmail.com",
