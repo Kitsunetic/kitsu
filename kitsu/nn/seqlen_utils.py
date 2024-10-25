18b4aff1c0e9a7fde8416fad0ac8c3fabd18e4ea
Kitsunetic <jh.shim.gg@gmail.com>
2024-10-25 16:30:33 +0900

    add padding_index function

--- kitsu/nn/seqlen_utils.py
+++ kitsu/nn/seqlen_utils.py
@@ -1,9 +1,12 @@
+import random
+from typing import Tuple
+
 import torch as th
 import triton
 import triton.language as tl
 from torch import Tensor
 
-__all__ = ["seqlen_to_index", "seqlen_to_batch_index"]
+__all__ = ["seqlen_to_index", "seqlen_to_batch_index", "padding_index"]
 
 
 @triton.jit
@@ -59,4 +62,46 @@ def seqlen_to_batch_index(seqlen: Tensor, max_seqlen: int):
     idx = seqlen.new_empty(seqlen[-1].item(), dtype=th.int64)
     BLK = triton.next_power_of_2(max_seqlen)
     seqlen_to_batch_index_kernel[(B,)](seqlen, idx, BLK)
+    return idx
+
+
+@triton.jit
+def padding_index_kernel(seqlen_ptr, new_seqlen_ptr, new_max_seqlen, idx_ptr, seed, BLK_N: tl.constexpr):
+    pid_b = tl.program_id(0)
+
+    i1 = tl.load(seqlen_ptr + pid_b)
+    j1 = tl.load(seqlen_ptr + pid_b + 1)
+    i2 = tl.load(new_seqlen_ptr + pid_b)
+    j2 = tl.load(new_seqlen_ptr + pid_b + 1)
+    l1, l2 = j1 - i1, j2 - i2
+
+    for pid_n in range(tl.cdiv(new_max_seqlen, BLK_N)):
+        idx = pid_n * BLK_N + tl.arange(0, BLK_N)  # n
+        idx64 = idx.to(tl.int64)
+        rnd64 = (tl.rand(seed, idx) * l1).to(tl.int64)  # n, [0, l1 - 1]
+        val = tl.where(idx >= l1, rnd64, idx64)
+        tl.store(idx_ptr + i2 + idx, val, mask=idx < l2)
+
+
+def padding_index(seqlen: Tensor, window_size: int) -> Tensor:
+    """
+    Args:
+        seqlen (Tensor): (batch_size + 1,), int32.
+        window_size (int):
+    Returns:
+        idx (Tensor): (M, ), int64
+    """
+    B = seqlen.size(0) - 1
+    seed = int(random.random() * 1e6)
+
+    pad_size = ((seqlen[:-1] - seqlen[1:]) % window_size).cumsum_(0)
+    new_seqlen = seqlen.clone()
+    new_seqlen[1:] += pad_size
+    new_max_seqlen = (new_seqlen[1:] - new_seqlen[:-1]).amax().item()
+    new_N = new_seqlen[-1].item()
+    idx = th.empty(new_N, dtype=th.int64, device=seqlen.device)
+
+    BLK_N = min(32, max(2048, triton.next_power_of_2(new_max_seqlen)))
+    grid = (B,)
+    padding_index_kernel[grid](seqlen, new_seqlen, new_max_seqlen, idx, seed, BLK_N=BLK_N)
     return idx
