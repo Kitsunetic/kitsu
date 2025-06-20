3f72bd42b5223817ef99f993e35c01cff612c6e1
Kitsunetic <jh.shim.gg@gmail.com>
2025-06-19 17:13:52 -0700

    add PadRight

--- kitsu/utils/torch.py
+++ kitsu/utils/torch.py
@@ -2,7 +2,7 @@ import gc
 
 import numpy as np
 import torch as th
-from torch import Tensor
+from torch import Tensor, FloatTensor
 
 
 def get_device():
@@ -59,3 +59,26 @@ def get_mixed_precision_dtype(device):
     else:
         # On CPU, bfloat16 is supported but autocast for CPU is less common
         return th.float16
+
+
+class PadRight:
+    def __init__(self, x, attn_mask):
+        self.original_mask = attn_mask
+        self.x_shape = x.shape
+        self.indices1 = attn_mask.nonzero(as_tuple=True)[0]
+        self.indices2 = sum([list(range(n)) for n in attn_mask.sum(1).tolist()], [])
+
+        self.new_attn_mask = th.zeros_like(attn_mask)
+        self.new_attn_mask[self.indices1, self.indices2] = True
+
+    def pad(self, x: FloatTensor):
+        x_flat = x.contiguous()
+        y = th.zeros_like(x_flat)
+        y[self.indices1, self.indices2] = x_flat[self.original_mask]
+        return y
+
+    def unpad(self, y: FloatTensor):
+        y_flat = y.contiguous()
+        x = y.new_zeros(self.x_shape)
+        x[self.original_mask] = y_flat[self.new_attn_mask]
+        return x
