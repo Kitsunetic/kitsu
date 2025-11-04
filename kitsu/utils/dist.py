6b712d62b4983768bac7943c0e70a48b0c34153b
Kitsunetic <jh.shim.gg@gmail.com>
2025-11-04 03:52:13 +0000

    add safe_all_gather_object, safe_all_gather_list_object functions

--- kitsu/utils/dist.py
+++ kitsu/utils/dist.py
@@ -1,12 +1,16 @@
-from typing import Callable
+from functools import reduce
+from typing import Any, Callable, List
 
 import torch
 import torch.distributed as dist
+from torch import Tensor
 
 __all__ = [
     "safe_all_reduce",
     "safe_all_mean",
     "safe_all_gather",
+    "safe_all_gather_object",
+    "safe_all_gather_list_object",
     "safe_barrier",
     "safe_broadcast",
     "is_rankzero",
@@ -27,11 +31,27 @@ def safe_all_mean(x) -> torch.Tensor:
     return x
 
 
-def safe_all_gather(x, dim=0) -> torch.Tensor:
+def safe_all_gather(x: Tensor, dim=0) -> torch.Tensor:
     if dist.is_initialized():
         xs = [torch.empty_like(x) for _ in range(dist.get_world_size())]
         dist.all_gather(xs, x)
         x = torch.cat(xs, dim=dim)
+    return x
+
+
+def safe_all_gather_object(x: Any) -> List[Any]:
+    if dist.is_initialized():
+        xs = [None] * dist.get_world_size()
+        dist.all_gather_object(xs, x)
+        x = xs
+    return x
+
+
+def safe_all_gather_list_object(x: List[Any]) -> List[Any]:
+    if dist.is_initialized():
+        xs = [None] * dist.get_world_size()
+        dist.all_gather_object(xs, x)
+        x = reduce(lambda a, b: a + b, xs, [])
     return x
 
 
